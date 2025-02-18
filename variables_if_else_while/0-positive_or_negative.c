98739761eb7d13c2ead1280c443335fc0b33ebea
Agustin Vidal <agustinvm88@gmail.com>
2025-02-18 14:52:42 +0000

    Add 0-positive_or_negative

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+/**
+*main - principal funtion
+*Return: return 0 if the program runs without problems
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is positive\n", n);
+	}
+
+	return (0);
+
+}
---
+++ variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This READER.md file will describe all the tasks on the project C-Variables, if, else, while.
