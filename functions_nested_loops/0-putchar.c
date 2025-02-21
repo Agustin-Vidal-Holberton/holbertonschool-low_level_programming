7ef7b165124b420deae2ff277bce85c425d5f97e
Agustin Vidal <agustinvm88@gmail.com>
2025-02-21 00:20:27 +0000

    Add 0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*main - Principal funtion
+*Description: Prints "_putchar" followed by a new line
+*Return: return 0 if its all runs ok
+*/
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+This README file will lists the programs that are on the project C-Funtions, nested loops
+0-putchar: This program will print _putchar followed by a new line
