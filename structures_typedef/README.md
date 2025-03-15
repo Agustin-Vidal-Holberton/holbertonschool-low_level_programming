3d23a92c853b2914d40c0d56660219d9f5fece78
Agustin Vidal <agustinvm88@gmail.com>
2025-03-15 22:10:41 +0000

    Add dog.h

---
+++ structures_typedef/README.md
@@ -0,0 +1,3 @@
+This file will list all the task of the C - Structures, typedef project:
+
+dog.h: Define a new type struct dog
---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - estructura que almacena informacion de un perro
+ * @name: nombre del perro(char)
+ * @age: edad del perro (float)
+ * @owner: due√o del perro (char)
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H*/
