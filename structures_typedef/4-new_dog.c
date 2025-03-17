9a9802ed78d9e50728d1c538f9ab5f1923429fe8
Agustin Vidal <agustinvm88@gmail.com>
2025-03-17 00:42:44 +0000

    Add 4-new_dog.c 2

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,7 +1,39 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+/**
+ * _strcpy - Copia una cadena de origen a destino
+ * @dest: Puntero al destino
+ * @src: Puntero a la cadena de origen
+ * Return: Puntero a destino
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+/**
+ * _strlen - Calcula la longitud de una cadena
+ * @s: Cadena de caracteres
+ * Return: Longitud de la cadena
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
 /**
 * new_dog - Crea un nuevo perro.
 * @name: Nombre del perro.
@@ -11,38 +43,34 @@
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog = malloc(sizeof(dog_t));
+	dog_t *dog;
+	char *name_copy, *owner_copy;
+	int name_len, owner_len;
 
+	dog = malloc(sizeof(dog_t));
 	if (!dog)
 	{
 		return (NULL);
 	}
-	if (name)
+	name_len = _strlen(name);
+	owner_len = _strlen(owner);
+	name_copy = malloc(name_len + 1);
+	if (!name_copy)
 	{
-		dog->name = malloc(strlen(name) + 1);
-		if (!dog->name)
-		{
-			return (free(dog), NULL);
-		}
-		strcpy(dog->name, name);
+		free(dog);
+		return (NULL);
 	}
-	else
+	_strcpy(name_copy, name);
+	owner_copy = malloc(owner_len + 1);
+	if (!owner_copy)
 	{
-		dog->name = NULL;
+		free(name_copy);
+		free(dog);
+		return (NULL);
 	}
-	if (owner)
-	{
-		dog->owner = malloc(strlen(owner) + 1);
-		if (!dog->owner)
-		{
-			return (free(dog->name), free(dog), NULL);
-		}
-		strcpy(dog->owner, owner);
-	}
-	else
-	{
-		dog->owner = NULL;
-	}
+	_strcpy(owner_copy, owner);
+	dog->name = name_copy;
 	dog->age = age;
+	dog->owner = owner_copy;
 	return (dog);
 }
--- structures_typedef/README.md
+++ structures_typedef/README.md
@@ -4,4 +4,4 @@ dog.h: Define a new type struct dog
 1-init_dog.c: function that initialize a variable of type struct dog
 2-print_dog.c: function that prints a struct dog
 dog.h : Define a new type dog_t as a new name for the type struct dog.
-4-new_dog.c: Write a function that creates a new dog
+4-new_dog.c: Write a function that creates a new dog.
