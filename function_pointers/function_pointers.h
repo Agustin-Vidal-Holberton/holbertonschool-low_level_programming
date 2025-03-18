ea26d6eb31e6e993125d073503f09cec07cdcf03
Agustin Vidal <agustinvm88@gmail.com>
2025-03-18 21:54:29 +0000

    Add 2-int_index.c

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -6,6 +6,7 @@
 /*Prototipo de funciones*/
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 /*Definicion de un nuevo tipo dog_t como alias de struct dog*/
 
