940a798fc105f1c7daa30ddc0d2902e338b18e0e
Agustin Vidal <agustinvm88@gmail.com>
2025-03-18 22:55:52 +0000

    Add 3-main.c, 3-calc.h, 3-get_op_func.c, 3-op_functions.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,18 +1,31 @@
-#include <stdio.h>
-#include "dog.h"
+#include "3-calc.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * main - programa principal para calcular operaciones basicas.
+ * @argc: Numero de argumentos pasados al programa
+ * @argv: Array de cadenas de los argumentos pasados
+ * Return: 0 en caso de exito, otro valor en caso de error.
  */
-int main(void)
+int main(int argc, char *argv[])
 {
-    dog_t my_dog;
+	int num1, num2, result;
+	int (*operation)(int, int);
+	
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    my_dog.name = "Poppy";
-    my_dog.age = 3.5;
-    my_dog.owner = "Bob";
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-    return (0);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operation = get_op_func(argv[2]);
+	if (operation == NULL || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	result = operation(num1, num2);
+	printf("%d\n", result);
+	return (0);
 }
