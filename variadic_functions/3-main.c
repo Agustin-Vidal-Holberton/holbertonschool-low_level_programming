42489e58134ceed8b77a011cf3eb3af39c62e02b
Agustin Vidal <agustinvm88@gmail.com>
2025-03-24 01:44:47 +0000

    Add 3-print_all.c

--- variadic_functions/3-main.c
+++ variadic_functions/3-main.c
@@ -1,31 +1,12 @@
-#include "3-calc.h"
+#include "variadic_functions.h"
 
 /**
- * main - programa principal para calcular operaciones basicas.
- * @argc: Numero de argumentos pasados al programa
- * @argv: Array de cadenas de los argumentos pasados
- * Return: 0 en caso de exito, otro valor en caso de error.
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-int main(int argc, char *argv[])
+int main(void)
 {
-	int num1, num2, result;
-	int (*operation)(int, int);
-
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-	operation = get_op_func(argv[2]);
-	if (operation == NULL || argv[2][1] != '\0')
-	{
-		printf("Error\n");
-		exit(99);
-	}
-	result = operation(num1, num2);
-	printf("%d\n", result);
-	return (0);
+    print_all("ceis", 'B', 3, "stSchool");
+    return (0);
 }
