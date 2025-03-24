1c0aba98382161550d3d32ad8aabf0c7188cbf69
Agustin Vidal <agustinvm88@gmail.com>
2025-03-24 00:22:05 +0000

    Add 2-print_strings.c

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -5,6 +5,7 @@
 /*Prototipo de funciones*/
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 
 /*Definicion de un nuevo tipo dog_t como alias de struct dog*/
 
