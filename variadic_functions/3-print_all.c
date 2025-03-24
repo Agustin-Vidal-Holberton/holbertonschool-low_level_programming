1474718e4b098cbc43307289fcc20dcb044a2472
Agustin Vidal <agustinvm88@gmail.com>
2025-03-24 02:01:54 +0000

    Add 3-print_all.c 2

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -11,31 +11,31 @@ void print_all(const char * const format, ...)
 {
 	va_list args;
 	unsigned int i = 0;
-	char *str, *separator = "";
+	char *str;
+	char *separator = "";
 
 	va_start(args, format);
 
 	while (format && format[i])
 	{
-		printf("%s", separator);
 		switch (format[i])
 		{
 			case 'c':
-				printf("%c", va_arg(args, int));
+				printf("%s%c", separator, va_arg(args, int);
 				break;
 			case 'i':
-				printf("%d", va_arg(args, int));
+				printf("%s%d", separator, va_arg(args, int);
 				break;
 			case 'f':
-				printf("%f", va_arg(args, double));
+				printf("%s%d", separator, va_arg(args, double));
 				break;
 			case 's':
 				str = va_arg(args, char *);
-				if (str == NULL)
-				{
+				if (!str)
+				
 					str = "(nil)";
-				}
-				printf("%s", str);
+				
+				printf("%s%s", separator, str);
 				break;
 			default:
 				i++;
